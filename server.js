b1fdc8b1c45060c0500452c3c685a7d077780610
SoulNinja-dev <aadhi123awesome@gmail.com>
2023-01-05 01:19:46 +0530

    add captcha

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-import express from 'express'
-import { config } from 'dotenv'
-import mongoose from 'mongoose'
-import morgan from 'morgan'
-import user from './routes/api/user.js'
-import tracks from './routes/api/tracks.js'
-import cors from "cors"
-config({ path: './config/.env' })
+import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import user from "./routes/api/user.js";
+import tracks from "./routes/api/tracks.js";
+import cors from "cors";
+config({ path: "./config/.env" });
 
-const app = express()
+const app = express();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, (err) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log('Connected to MongoDB')
+    console.log("Connected to MongoDB");
   }
-})
+});
 
 // Express configuration
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(cors());
 
 // @route   GET /
 // @desc    Test Base API
 // @access  Public
-app.get('/', (_req, res) => {
-  res.send('api up and running')
-})
+app.get("/", (_req, res) => {
+  res.send("api works");
+});
 
-app.use('/api/user', user)
-app.use('/api/tracks', tracks)
+app.use("/api/user", user);
+app.use("/api/tracks", tracks);
 
-const port = process.env.PORT || 1717
-app.listen(port, () => console.log(`Server started on port ${port}`))
+const port = process.env.PORT || 1717;
+app.listen(port, () => console.log(`Server started on port ${port}`));
